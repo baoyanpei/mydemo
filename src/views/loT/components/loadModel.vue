9662e34c7028fbcdbbf6b9d3e0085c57fabf648d
hanxiang <3348047@qq.com>
2019-04-12 17:01:15 +0800

    优化加载

--- src/views/loT/components/loadModel.vue
+++ src/views/loT/components/loadModel.vue
@@ -47,7 +47,10 @@
 
         project_id: 10000,
         building_id: 87, // 87 周边
-        modIDList: []
+        modIDAPIList: [],
+        modIDList: [],
+        totalNeedModel: 0,
+        totalOKModel: 0
       }
     },
     computed: {
@@ -63,12 +66,12 @@
       // modelByID() {
       //   return this.$store.state.model3d.modelByID
       // },
-      modelMap() {
-        return this.$store.state.model3d.modelMap
-      },
-      buildMap() {
-        return this.$store.state.model3d.buildMap
-      },
+      // modelMap() {
+      //   return this.$store.state.model3d.modelMap
+      // },
+      // buildMap() {
+      //   return this.$store.state.model3d.buildMap
+      // },
 
     },
     created() {
@@ -83,18 +86,56 @@
       // 通过赋值null的方式，释放掉worker引用，这样就可以关闭worker，这是作者在github上说的
       this.worker = null
     },
-    watch: {},
+    watch: {
+      async totalOKModel(curVal, oldVal) {
+        // console.log('curVal', curVal)
+        if (curVal === this.totalNeedModel) {
+          console.log('-- this.modIDAPIList', this.modIDAPIList)
+
+          let _chunkList = lodash.chunk(this.modIDAPIList, 100)
+          console.log('_chunkList', _chunkList)
+          for (let i = 0, len = _chunkList.length; i < len; i++) {
+            let ids = _chunkList[i].join(',')
+            // console.log('ids',ids)
+            await this.getModelFromAPI(ids)
+          }
+
+        }
+      }
+
+    },
     async mounted() {
-      this.loadingDialog = this.$loading({
-        // lock: true,
-        // text: '正在读取数据...',
-        // spinner: 'el-icon-loading',
-        // background: 'rgba(0, 0, 0, 0.5)',
-        // customClass: 'loading-class',
-        target: document.querySelector('.treeDiv')
-      });
+      // this.loadingDialog = this.$loading({
+      //   // lock: true,
+      //   // text: '正在读取数据...',
+      //   // spinner: 'el-icon-loading',
+      //   // background: 'rgba(0, 0, 0, 0.5)',
+      //   // customClass: 'loading-class',
+      //   target: document.querySelector('.treeDiv')
+      // });
       await this.getBuildingListByProID()
-      console.log('123')
+      // console.log('this.modIDList', this.modIDList)
+
+      this.totalNeedModel = this.modIDList.length
+      for (let i = 0, len = this.modIDList.length; i < len; i++) {
+        let model_id = this.modIDList[i]
+        IndexedDB.getData(this.modelDB, 'model', model_id, async (result) => {
+          if (result) {
+            this.$emit('unitGroupAddMesh', result.mesh, result.modelID, result.unit)
+            // this.modIDIndexList.push(model_id)
+
+          } else {
+
+            // this.getModelFromAPI(model_id)
+            // console.log('model_id', model_id)
+            this.modIDAPIList.push(model_id)
+
+          }
+          this.totalOKModel++
+        });
+      }
+
+      console.log('123', this.modIDAPIList.length)
 
     },
     methods: {
@@ -105,17 +146,25 @@
             method: 'GetBuildingListByProID',
             project_id: this.project_id
           }
-          this.$store.dispatch('QueryBuildingListByProID', param).then(() => {
-            this.buildListByProID.forEach(async build => {
-              if (build.ID === 87 || build.ID === 86 ) {//|| build.ID === 88 || build.ID === 89
-                const _floorList = await this.getFloorListByBuildingID(build.ID)
-                console.log('this.modIDList', this.modIDList)
-
-
+          this.$store.dispatch('QueryBuildingListByProID', param).then(async () => {
+            // console.log('buildList', buildList)
+            for (let i = 0, len = this.buildListByProID.length; i < len; i++) {
+              let build = this.buildListByProID[i]
+              if (build.ID === 87 || build.ID === 86) { //|| build.ID === 88 || build.ID === 89
+                const _floorIDList = await this.getFloorListByBuildingID(build.ID)
+                // console.log('_floorIDList', _floorIDList)
+                this.modIDList = [...this.modIDList, ..._floorIDList]
+                // this.modIDList.push(model_id)
               }
-              resolve()
-              this.loadingDialog.close()
-            });
+            }
+            // this.buildListByProID.forEach(async build => {
+            //   if (build.ID === 87 || build.ID === 86) { //|| build.ID === 88 || build.ID === 89
+            //     const _floorList = await this.getFloorListByBuildingID(build.ID)
+            //   }
+            // });
+
+            resolve()
+            // this.loadingDialog.close()
           }).catch((e) => {
             console.log("e", e)
             resolve()
@@ -134,18 +183,28 @@
           }
           this.$store.dispatch('QueryFloorListByProID', param).then(async () => {
             // console.log("this.floorListByBudID", this.floorListByBudID)
-            this.optionsProjectPersion = this.projectPersonList
-            let listData = []
-            this.floorListByBudID.forEach(async floor => {
+            // this.optionsProjectPersion = this.projectPersonList
+            // let listData = []
+
+
+
+            // for (let i = 0, len = this.floorListByBudID.length; i < len; i++) {
+            //   console.log('this.floorListByBudID');
+            //   await wait(1000);
+            // }
+            // this.floorListByBudID.forEach(async floor => {
+            let idList = []
+            for (let i = 0, len = this.floorListByBudID.length; i < len; i++) {
+              let floor = this.floorListByBudID[i]
               // console.log('floor', floor)
               // if (floor.NAME === '2F(4.200-8.400)' || floor.NAME === '3F(8.400-12.600)') {
               floor.BUILDID = building_id
               const _modList = await this.getModListByFloorID(floor)
               // console.log('_modList', _modList)
               // this.$emit('unitTotalAdd', _modList.length)
-              let idList = []
+
               _modList.forEach(mod => {
-                if (building_id === 86 || building_id === 88 || building_id === 89) {//
+                if (building_id === 86 || building_id === 88 || building_id === 89) { //
                   let _PARAMS_TYPE = []
                   if (mod.PARAMS !== "") {
                     _PARAMS_TYPE = mod.PARAMS.split(',')
@@ -153,24 +212,24 @@
                     if (_match.length > 0) {
                       // console.log('mod', mod)
                       idList.push(mod.ID)
-                      // this.getModdelByID(mod.ID)
                     }
                   }
 
                 } else {
                   idList.push(mod.ID)
-                  // this.getModdelByID(mod.ID)
                 }
-                // this.modIDList.push(mod.ID)
               })
-              this.$emit('unitTotalAdd', idList.length)
-              idList.forEach(id => {
-                // 加载模型
-                this.getModdelByID(id)
-              })
-              // }
-            });
-            resolve(listData)
+
+
+
+            };
+            this.$emit('unitTotalAdd', idList.length)
+            // for (let i = 0, len = idList.length; i < len; i++) {
+            //   let _id = idList[i]
+            //   // await this.getModdelByID(_id)
+            //   this.getModdelByID(_id)
+            // }
+            resolve(idList)
           }).catch((e) => {
             console.log("e", e)
             resolve([])
@@ -190,19 +249,11 @@
           // console.log('GetModListByFloorID', param)
           this.$store.dispatch('QueryModListByFloorID', param).then(() => {
             this.modListByFloorID.forEach(d => {
-              // if (parentId === 0) {
-              //   //是根元素的hua ,不做任何操作,如果是正常的for-i循环,可以直接continue.
-              // } else {
-              //如果ele是子元素的话 ,把ele扔到他的父亲的child数组中.
               // flag=1 可以画
               if (d.FLAG === 1) {
                 _modList.push(d)
               }
             });
-            //去除重复元素
-            // listData = listData.filter(ele => ele.PARENT_ID === 0);
-
-            // console.log("resolve", listData)
             resolve(_modList)
           }).catch((e) => {
             console.log("e", e)
@@ -213,50 +264,59 @@
       },
       //根据模型ID获取模型信息
       getModdelByID(model_id) {
-        IndexedDB.getData(this.modelDB, 'model', model_id, (result) => {
-          if (result) {
-            // this.$emit('unitTotalAdd', 1)
-            this.$emit('unitGroupAddMesh', result.mesh, result.modelID, result.unit)
-          } else {
+        return new Promise((resolve, reject) => {
+          IndexedDB.getData(this.modelDB, 'model', model_id, (result) => {
+            if (result) {
+              // this.$emit('unitTotalAdd', 1)
+              this.$emit('unitGroupAddMesh', result.mesh, result.modelID, result.unit)
+              // this.modIDIndexList.push(model_id)
+            } else {
 
-            this.getModelFromAPI(model_id)
+              // this.getModelFromAPI(model_id)
+              // console.log('model_id', model_id)
+              this.modIDList.push(model_id)
+            }
+            resolve()
+          });
+        })
 
-          }
-        });
         // });
 
       },
       getModelFromAPI(model_id) {
-        const param = {
-          method: 'GetModelByID',
-          project_id: this.project_id,
-          model_id: model_id
-        }
-        this.$store.dispatch('QueryModdelByID', param).then((data) => {
-          data.forEach(unit => {
-            // console.log('unit', unit)
-            if (unit.MESH_JSON !== '') {
-              // this.$emit('unitTotalAdd', 1)
-              let meshJson = getOriMesh(unit.MESH_JSON)
-              let modelData = {
-                modelID: unit.ID,
-                unit: unit,
-                mesh: meshJson
+        return new Promise((resolve, reject) => {
+          const param = {
+            method: 'GetModelByID',
+            project_id: this.project_id,
+            model_id: model_id
+          }
+          this.$store.dispatch('QueryModdelByID', param).then((data) => {
+            data.forEach(unit => {
+              // console.log('unit', unit)
+              if (unit.MESH_JSON !== '') {
+                // this.$emit('unitTotalAdd', 1)
+                let meshJson = getOriMesh(unit.MESH_JSON)
+                let modelData = {
+                  modelID: unit.ID,
+                  unit: unit,
+                  mesh: meshJson
+                }
+                this.$emit('unitGroupAddMesh', meshJson, unit.ID, unit)
+                let worker = new Worker("/static/workIndexedDB.js");
+                worker.postMessage(modelData); //向worker发送数据
+                worker.onmessage = function (evt) { //接收worker传过来的数据函数
+                  worker.terminate();
+                }
+              } else {
+                this.$emit('unitGroupAddMesh', null, null, null)
               }
-              this.$emit('unitGroupAddMesh', meshJson, unit.ID, unit)
-              let worker = new Worker("/static/workIndexedDB.js");
-              worker.postMessage(modelData); //向worker发送数据
-              worker.onmessage = function (evt) { //接收worker传过来的数据函数
-                worker.terminate();
-              }
-            } else {
-              this.$emit('unitGroupAddMesh', null, null, null)
-            }
-
-          });
-        }).catch((e) => {
-          console.log(e)
+              resolve()
+            });
+          }).catch((e) => {
+            console.log(e)
+          })
         })
+
       }
     }
   }
