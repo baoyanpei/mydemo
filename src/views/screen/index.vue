debcee3475164dcf391c04d81fa47b048b332d65
hanxiang <3348047@qq.com>
2019-04-28 16:27:24 +0800

    门禁管理

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -8,22 +8,31 @@
       <div class="header"></div>
     </el-row>
     <el-row>
-      <el-col :span="4">
+      <el-col :span="5">
         <div class="gate-area">
           <div class="title">
             <img src="/static/screen/title-bg1.png" class="bg" />
-            <div class="text" >门禁管理</div>
+            <div class="text">门禁管理</div>
           </div>
           <div class="main">
-            123123
+            <GateArea ref="gateArea"></GateArea>
+          </div>
+        </div>
+        <div class="inout-area">
+          <div class="title">
+            <img src="/static/screen/title-bg1.png" class="bg" />
+            <div class="text">场内人员</div>
+          </div>
+          <div class="main">
+            123
           </div>
         </div>
       </el-col>
       <el-col :span="10">
-        
+
       </el-col>
-      <el-col :span="10">
-        
+      <el-col :span="9">
+
       </el-col>
     </el-row>
   </div>
@@ -31,16 +40,17 @@
 
 <script>
   // import $ from 'jquery'
+  import GateArea from './components/gateArea'
   export default {
     directives: {},
     name: 'Main',
     components: {
-
+      GateArea
       // VueDragResize
     },
     data() {
       return {
-
+        
       }
     },
     computed: {
@@ -53,10 +63,6 @@
 
     },
     mounted() {
-      console.log("inde123x mount")
-
-
-
     },
     destroyed() {},
     methods: {
