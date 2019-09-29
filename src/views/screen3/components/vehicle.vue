d4cf4a02a9a3c17ceb5f8b040df44320b89f45aa
hanxiang <3348047@qq.com>
2019-09-29 17:25:27 +0800

    车辆

--- src/views/screen3/components/vehicle.vue
+++ src/views/screen3/components/vehicle.vue
@@ -7,8 +7,49 @@
   <div class="screen-vehicle">
     <div v-if="hasVehicleDevice === false" class="noVehicleDeviceTips">{{noVehicleDeviceTips}}</div>
     <div v-if="hasVehicleDevice === true">
+
       <el-row>
-        <el-col :span="9">
+        <el-col :span="12">
+          <div style='padding-left: 10px;' class="last-vehicle">
+            <div class="title">最新进场车牌号码</div>
+            <div class="lisence">
+              <div class="lisence-letter">{{lastVehicle.lisence[0]}}</div>
+              <div class="lisence-letter">{{lastVehicle.lisence[1]}}</div>
+              <div class="lisence-letter">{{lastVehicle.lisence[2]}}</div>
+              <div class="lisence-letter">{{lastVehicle.lisence[3]}}</div>
+              <div class="lisence-letter">{{lastVehicle.lisence[4]}}</div>
+              <div class="lisence-letter">{{lastVehicle.lisence[5]}}</div>
+              <div class="lisence-letter">{{lastVehicle.lisence[6]}}</div>
+            </div>
+            <div class="in-time">进场时间：{{lastVehicle.created_time}}</div>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <img :src="lastVehicle.pic" class="vehicle-img" />
+        </el-col>
+      </el-row>
+      <el-row>
+        <table>
+          <tbody>
+            <tr>
+              <td>甘M30989 10/20 20:39</td>
+              <td>甘M30989 10/20 20:39</td>
+            </tr>
+            <tr>
+              <td>甘M30989 10/20 20:39</td>
+              <td>甘M30989 10/20 20:39</td>
+            </tr>
+            <tr>
+              <td>甘M30989 10/20 20:39</td>
+              <td>甘M30989 10/20 20:39</td>
+            </tr>
+            <tr>
+              <td>甘M30989 10/20 20:39</td>
+              <td>甘M30989 10/20 20:39</td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- <el-col :span="9">
           <el-table class="screen-vehicle-table" ref="vehicleTable" stripe :data="vehicleList" height="320px"
             :empty-text="vehicleTableEmptyText" style="width: 100%" size="mini" :show-header="true" border
             :row-class-name="tableRowClassName" :resizabl="false">
@@ -22,8 +63,8 @@
             </el-table-column>
 
           </el-table>
-        </el-col>
-        <el-col :span="15">
+        </el-col> -->
+        <!-- <el-col :span="15">
           <div style='padding-left: 10px;' class="last-vehicle">
             <div class="title">最新进场车牌号码</div>
             <div class="lisence">
@@ -40,7 +81,7 @@
             <img :src="lastVehicle.pic" class="vehicle-img" />
           </div>
 
-        </el-col>
+        </el-col> -->
       </el-row>
     </div>
 
