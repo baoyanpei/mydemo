192d461d8d9872570b9b3259a8ad21b789e9c879
hanxiang <3348047@qq.com>
2019-10-09 15:06:29 +0800

    任务的显示

--- src/views/screen3/components/tasks.vue
+++ src/views/screen3/components/tasks.vue
@@ -5,8 +5,35 @@
 
 <template>
   <div class="screen-tasks">
+    <div v-for="(item, i) in AllInstList" class="task-item">
+      <div class="task-left">
+        <img class="task-img" v-bind:src="item.imgSrc">
+      </div>
+      <div class="task-right">
+        <div class="task-detail1">
+          <div class="task-title">
+            {{item.title}}
+          </div>
+          <div class="task-status">
+            <span v-if="item.nodeId === 'Node2'" class="node2">待认领</span>
+            <span v-if="item.nodeId === 'Node3'" class="node3">待处理</span>
+            <span v-if="item.nodeId === 'Node5'" class="node5">待质检</span>
+            <span v-if="item.nodeId === 'Node1' || item.nodeId === 'End4'" class="node1">已完成</span>
+          </div>
+        </div>
 
-    screen-tasks
+
+        <div class="task-detail2">
+          <div class="task-type" v-html='item.typeDesc'></div>
+          <div class="task-creator">
+            发起人：{{item.creatorName}}
+          </div>
+          <div class="task-time">
+            {{item.sendTime}}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -17,6 +44,22 @@
     data() {
       return {
         project_id: null,
+        AllInstList: [],
+        typecolor: {
+          "安全类": ["#EEB422"],
+          "安全": ["#EEB422"],
+          "质量": ["#00CD66"],
+          "技术": ["#0000CD"],
+          "施工": ["#388E8E"],
+          "资料": ["#8B4513"],
+          "财务": ["#CD3333"],
+          "物资": ["#8968CD"],
+          "劳务": ["#838B8B"],
+          "法务": ["#262626"],
+          "预算": ["#00E5EE"],
+          "综合": ["#CDB79E"],
+          "平台技术": ["#00B2EE"]
+        },
 
       }
     },
@@ -43,23 +86,39 @@
       init(project_id) {
         this.project_id = project_id
         this.getAllInstList()
-
+        this.refreshData()
 
       },
+      refreshData() {
+        setTimeout(() => {
+          this.getAllInstList()
+          this.refreshData()
+          // console.log('getVehicleGateData')
+        }, 60 * 1000)
+      },
       getAllInstList() {
-
+        // this.AllInstList = []
         let param = {
           method: 'get_all_inst_list',
           project_id: this.project_id,
           page: 1,
-          limit: 20,
+          limit: 3,
           qtype: 'inProgress',
           flow_id: 'ProblemFindSolve01'
         }
 
-        this.$store.dispatch('GetAllInstList', param).then((vehicleDataList) => {
-          console.log('vehicleDataList', vehicleDataList)
-
+        this.$store.dispatch('GetAllInstList', param).then((AllInstList) => {
+          console.log('AllInstList', AllInstList)
+          if (AllInstList.count > 0) {
+            this.AllInstList = AllInstList.data
+          }
+          this.AllInstList.forEach(item => {
+            let _imgSrc = `https://buskey.cn/api/oa/workflow/thumbnail.jpg?work_id=${item.workId}&w=150`
+            item['imgSrc'] = _imgSrc
+            let _typeColor = this.typecolor[item.questions_type][0]
+            item['typeDesc'] = `<span style='background-color:${_typeColor}'>${item.questions_type}</span>`
+          })
+          console.log('this.AllInstList', this.AllInstList)
         }).catch(() => {
           //   this.loading = false
         })
