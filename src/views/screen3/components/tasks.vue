7d2164e4cd237381083cd30eaea0087c791a51ee
hanxiang <3348047@qq.com>
2019-10-10 12:37:09 +0800

    修改数据提取

--- src/views/screen3/components/tasks.vue
+++ src/views/screen3/components/tasks.vue
@@ -18,7 +18,7 @@
             <span v-if="item.nodeId === 'Node2'" class="node2">待认领</span>
             <span v-if="item.nodeId === 'Node3'" class="node3">待处理</span>
             <span v-if="item.nodeId === 'Node5'" class="node5">待质检</span>
-            <span v-if="item.nodeId === 'Node1' || item.nodeId === 'End4'" class="node1">已完成</span>
+            <span v-if="item.nodeId === 'Node1' || item.nodeId === 'End4' || item.nodeId === undefined" class="node1">已完成</span>
           </div>
         </div>
 
@@ -83,45 +83,92 @@
 
     },
     methods: {
-      init(project_id) {
+      async init(project_id) {
         this.project_id = project_id
-        this.getAllInstList()
+
+
+        await this.getAllInstList()
         this.refreshData()
 
       },
       refreshData() {
-        setTimeout(() => {
-          this.getAllInstList()
+        setTimeout(async () => {
+          await this.getAllInstList()
           this.refreshData()
           // console.log('getVehicleGateData')
         }, 60 * 1000)
       },
-      getAllInstList() {
-        // this.AllInstList = []
-        let param = {
-          method: 'get_all_inst_list',
-          project_id: this.project_id,
-          page: 1,
-          limit: 3,
-          qtype: 'inProgress',
-          flow_id: 'ProblemFindSolve01'
-        }
+      getInProgressInstList() {
+        return new Promise((resolve, reject) => {
+          let param = {
+            method: 'get_all_inst_list',
+            project_id: this.project_id,
+            page: 1,
+            limit: 300,
+            qtype: 'inProgress',
+            flow_id: 'ProblemFindSolve01'
+          }
 
-        this.$store.dispatch('GetAllInstList', param).then((AllInstList) => {
-          console.log('AllInstList', AllInstList)
-          if (AllInstList.count > 0) {
-            this.AllInstList = AllInstList.data
+          this.$store.dispatch('GetAllInstList', param).then((InProgressList) => {
+            // console.log('InProgressList', InProgressList)
+            resolve(InProgressList)
+          }).catch(() => {
+            //   this.loading = false
+          })
+
+        })
+
+      },
+      getHasDoneInstList() {
+        return new Promise((resolve, reject) => {
+          let param = {
+            method: 'get_all_inst_list',
+            project_id: this.project_id,
+            page: 1,
+            limit: 30,
+            qtype: 'hasDone',
+            flow_id: 'ProblemFindSolve01'
+          }
+
+          this.$store.dispatch('GetAllInstList', param).then((HasDoneList) => {
+            // console.log('HasDoneList', HasDoneList)
+            resolve(HasDoneList)
+          }).catch(() => {
+            //   this.loading = false
+          })
+
+        })
+
+      },
+      getAllInstList() {
+        return new Promise(async (resolve, reject) => {
+
+          let _InProgressInstData = await this.getInProgressInstList()
+          let _getHasDoneInstData = await this.getHasDoneInstList()
+          console.log('_InProgressInstData', _InProgressInstData)
+          console.log('_getHasDoneInstData', _getHasDoneInstData)
+          this.AllInstList = []
+          if (_InProgressInstData.count > 0) {
+            this.AllInstList = [...this.AllInstList, ..._InProgressInstData.data]
+          }
+          if (_getHasDoneInstData.count > 0) {
+            this.AllInstList = [...this.AllInstList, ..._getHasDoneInstData.data]
+          }
+          this.AllInstList = lodash.orderBy(this.AllInstList, ['sendTime'], ['desc']);
+          if (this.AllInstList.length > 3) {
+            this.AllInstList.splice(3)
           }
           this.AllInstList.forEach(item => {
-            let _imgSrc = `https://buskey.cn/api/oa/workflow/thumbnail.jpg?work_id=${item.workId}&w=150`
+            let _imgSrc = `https://buskey.cn/api/oa/workflow/thumbnail.jpg?work_id=${item.workId}&w=250`
             item['imgSrc'] = _imgSrc
             let _typeColor = this.typecolor[item.questions_type][0]
-            item['typeDesc'] = `<span style='background-color:${_typeColor}'>${item.questions_type}</span>`
+            item['typeDesc'] =
+              `<span style='background-color:${_typeColor}'>${item.questions_type}</span>`
           })
           console.log('this.AllInstList', this.AllInstList)
-        }).catch(() => {
-          //   this.loading = false
+          resolve()
         })
+
       },
     },
 
