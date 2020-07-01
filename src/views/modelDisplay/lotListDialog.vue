41e5fcf7578053e98b84f0498f22564a06f1d207
韩翔 <hanxiang@hanxiangdeMacBookPro2020.local>
2020-07-01 15:53:27 +0800

    添加标签

--- src/views/modelDisplay/lotListDialog.vue
+++ src/views/modelDisplay/lotListDialog.vue
@@ -1,24 +1,36 @@
 <style lang="scss">
-  @import "./lotListDialog";
-
+@import './lotListDialog';
 </style>
 <template>
-
   <div id="lot-list-dialog" class="lot-list-dialog">
-    <el-dialog :modal="false" width="450px" top="10vh" left="100" :lock-scroll="true" :close-on-click-modal="false"
-      :close-on-press-escape="true" :visible.sync="LotListDialog.show" @opened="openedDialogHandle"
-      @close="closeDialogHandle" :title="dialogTitle" v-el-drag-dialog>
-
+    <el-dialog
+      :modal="false"
+      width="450px"
+      top="10vh"
+      left="100"
+      :lock-scroll="true"
+      :close-on-click-modal="false"
+      :close-on-press-escape="true"
+      :visible.sync="LotListDialog.show"
+      @opened="openedDialogHandle"
+      @close="closeDialogHandle"
+      :title="dialogTitle"
+      v-el-drag-dialog
+    >
       <div class="device_list">
         <div v-show="tipMessage!==''" class="tip-message">{{tipMessage}}</div>
-        <el-row v-show="tipMessage===''" v-for="(item,index) in deviceList" :key="index" class="library-item">
+        <el-row
+          v-show="tipMessage===''"
+          v-for="(item,index) in deviceList"
+          :key="index"
+          class="library-item"
+        >
           <el-col :span="10" style="padding-left:5px;">
             <el-row :gutter="24">
               <div class="grid-content info-title">
                 <span class="info-title-link">{{item.device_name}}</span>
               </div>
             </el-row>
-
           </el-col>
           <el-col :span="14">
             <div class="view-point-button">
@@ -29,139 +41,122 @@
           </el-col>
         </el-row>
       </div>
-
     </el-dialog>
   </div>
-
 </template>
 
 <script>
-  let Base64 = require('js-base64').Base64
+let Base64 = require('js-base64').Base64
 
-  import {
-    Loading
-  } from 'element-ui';
+import { Loading } from 'element-ui'
 
-  export default {
-    components: {},
-    directives: {},
-    data() {
-      return {
-        loadingFull: false,
-        dialogTitle: '物联网设备库',
-        tipMessage: '',
-        buildItem: null, // 建筑模型信息
-        deviceList: []
-
+export default {
+  components: {},
+  directives: {},
+  data() {
+    return {
+      loadingFull: false,
+      dialogTitle: '物联网设备库',
+      tipMessage: '',
+      buildItem: null, // 建筑模型信息
+      deviceList: []
+    }
+  },
+  computed: {
+    project_id: {
+      get: function() {
+        return this.$store.state.project.project_id
+      },
+      set: function(newValue) {
+        this.$store.state.project.project_id = newValue
       }
     },
-    computed: {
-      project_id: {
-        get: function () {
-          return this.$store.state.project.project_id
-        },
-        set: function (newValue) {
-          this.$store.state.project.project_id = newValue
-        }
+    LotListDialog: {
+      get: function() {
+        return this.$store.state.loT.LotListDialog
       },
-      LotListDialog: {
-        get: function () {
-          return this.$store.state.loT.LotListDialog
-        },
-        set: function (newValue) {
-          this.$store.state.loT.LotListDialog = newValue
-        }
-      },
-
-    },
-    props: {
-
-    },
-    created: function () {
-
-    },
-    mounted() {
-      // const __PROJECT_ID = Cookies.get("PROJECT_ID")
-      // this.project_id = parseInt(__PROJECT_ID)
-
-    },
-    watch: {
-
-
-    },
-    methods: {
-      clearData() {
-        this.tipMessage = ""
-        this.deviceList = []
-        this.buildItem = null
-      },
-      async openedDialogHandle() {
-        // this.tipMessage = "正在查询ComponentLibraryListDialog"
-        this.buildItem = this.LotListDialog.buildItem
-        console.log('this.LotListDialog', this.LotListDialog)
-        this.tipMessage = "正在查询..."
-        this.deviceList = await this.getDeviceConfigList()
-        if (this.deviceList.length === 0) {
-          this.tipMessage = "当前建筑中没有绑定物联网设备模型"
-          return
-        }
-        this.tipMessage = ""
-      },
-      closeDialogHandle() {
-        this.clearData()
-      },
-
-      getDeviceConfigList() {
-        return new Promise((resolve, reject) => {
-          let itemList = []
-          const param = {
-            method: 'device_config',
-            project_id: this.project_id,
-            buliding_id: this.buildItem.item_id
-          }
-          this.$store.dispatch('GetDeviceConfig', param).then((_itemList) => {
-            console.log('GetDeviceConfig - _itemList', _itemList)
-            _itemList.forEach(item => {
-              if (item.device_type !== 13) {
-                itemList.push(item)
-              }
-            })
-            resolve(itemList)
-          })
-
-        })
-      },
-      editDeviceHandle(item) {
-        console.log('item', item)
-        let param = {
-          device: item
-        }
-        console.log('param', param)
-        this.$store.dispatch('SetLotDeviceEditChange', param).then((result) => {
-          this.closeDialog()
-        })
-
-      },
-      findDeviceHandle(item, type) {
-        console.log('item', item)
-        let param = {
-          device: item,
-          type: type
-        }
-        console.log('param', param)
-        this.$store.dispatch('SetLotDeviceFindChange', param).then((result) => {
-          // this.closeDialog()
-        })
-
-
-
-      },
-      closeDialog() {
-        this.$store.dispatch('ShowLotListDialog', {
-          show: false
-        }).then(() => {}).catch(() => {})
+      set: function(newValue) {
+        this.$store.state.loT.LotListDialog = newValue
       }
     }
-  }
+  },
+  props: {},
+  created: function() {},
+  mounted() {
+    // const __PROJECT_ID = Cookies.get("PROJECT_ID")
+    // this.project_id = parseInt(__PROJECT_ID)
+  },
+  watch: {},
+  methods: {
+    clearData() {
+      this.tipMessage = ''
+      this.deviceList = []
+      this.buildItem = null
+    },
+    async openedDialogHandle() {
+      // this.tipMessage = "正在查询ComponentLibraryListDialog"
+      this.buildItem = this.LotListDialog.buildItem
+      console.log('this.LotListDialog', this.LotListDialog)
+      this.tipMessage = '正在查询...'
+      this.deviceList = await this.getDeviceConfigList()
+      if (this.deviceList.length === 0) {
+        this.tipMessage = '当前建筑中没有绑定物联网设备模型'
+        return
+      }
+      this.tipMessage = ''
+    },
+    closeDialogHandle() {
+      this.clearData()
+    },
 
+    getDeviceConfigList() {
+      return new Promise((resolve, reject) => {
+        let itemList = []
+        const param = {
+          method: 'device_config',
+          project_id: this.project_id,
+          buliding_id: this.buildItem.item_id
+        }
+        this.$store.dispatch('GetDeviceConfig', param).then(_itemList => {
+          console.log('GetDeviceConfig - _itemList', _itemList)
+          _itemList.forEach(item => {
+            if (item.device_type !== 13 && item.device_type !== 12) {
+              itemList.push(item)
+            }
+          })
+          resolve(itemList)
+        })
+      })
+    },
+    editDeviceHandle(item) {
+      console.log('item', item)
+      let param = {
+        device: item
+      }
+      console.log('param', param)
+      this.$store.dispatch('SetLotDeviceEditChange', param).then(result => {
+        this.closeDialog()
+      })
+    },
+    findDeviceHandle(item, type) {
+      console.log('item', item)
+      let param = {
+        device: item,
+        type: type
+      }
+      console.log('param', param)
+      this.$store.dispatch('SetLotDeviceFindChange', param).then(result => {
+        // this.closeDialog()
+      })
+    },
+    closeDialog() {
+      this.$store
+        .dispatch('ShowLotListDialog', {
+          show: false
+        })
+        .then(() => {})
+        .catch(() => {})
+    }
+  }
+}
 </script>
