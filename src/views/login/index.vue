3eaed4674d51ba23c613c676bf797eada28cc0e0
韩翔 <hanxiang@hanxiankPro2020.lan>
2020-07-10 10:48:01 +0800

    改名字

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,7 +14,8 @@
 
       <div class="loginFormQrcode" v-show="loginType==='qrcode'">
         <!-- <img src="/static/login/title1.png" style="height:26px;" /> -->
-        <div class="title_name">甘肃建投建造信息管控平台</div>
+        <!--甘肃建投建造信息管控平台-->
+        <div class="title_name">建筑信息管控平台</div>
         <div id="qrcode_container"></div>
         <div class="qrcode_question">
           <router-link to="wxbindtip" replace style="color:#00b7d3">还未绑定手机号码？</router-link>
@@ -23,7 +24,8 @@
       <div class="loginFormContent" v-show="loginType==='mobile'">
         <div class="title-container">
           <!-- <img src="/static/login/title1.png" style="height:26px;" /> -->
-          <div class="title_name">甘肃建投建造信息管控平台</div>
+          <!--甘肃建投建造信息管控平台-->
+          <div class="title_name">建筑信息管控平台</div>
         </div>
         <div style="padding:0px 35px 0px 35px">
           <el-form-item prop="username" :rules="ruleUsername">
